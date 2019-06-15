e3f2b962f97b0b6ad96b116accc859475359ca44
Tommi Juslin <tommi.juslin@gmail.com>
2019-06-15 14:15:27 +0300

    anekdootit ja backend, step2

--- osa6/redux-anecdotes/src/App.js
+++ osa6/redux-anecdotes/src/App.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Notification from './components/Notification'
+import anecdoteService from './services/anecdotes'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
 
 const App = (props) => {
+  useEffect(() => {
+    anecdoteService
+      .getAll().then(anecdotes => props.initializeAnecdotes(anecdotes))
+  },[])
+
   return (
     <div>
       <h2>Anecdotes</h2>
@@ -14,4 +22,4 @@ const App = (props) => {
   )
 }
 
-export default App
+export default connect(null, { initializeAnecdotes })(App)
--- osa6/redux-anecdotes/src/components/AnecdoteForm.js
+++ osa6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -2,19 +2,23 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { showNotification, hideNotification } from '../reducers/notificationReducer'
+import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = (props) => {
 
-  const addAnecdote = (event) => {
+  const addAnecdote = async (event) => {
     event.preventDefault()
-    props.createAnecdote(event.target.anecdote.value)
-    props.showNotification(`new anecdote added '${event.target.anecdote.value}'`)
+    const content = event.target.anecdote.value
+    event.target.anecdote.value = ''
+    const newAnecdote = await anecdoteService.createNew(content)
+    props.createAnecdote(newAnecdote)
+
+    props.showNotification(`new anecdote added '${content}'`)
     setTimeout(() => {
       props.hideNotification(null)
     }, 5000)
-    event.target.anecdote.value = ''
-
   }
+
   return (
     <form onSubmit={addAnecdote}>
       <input name='anecdote' />
@@ -29,6 +33,4 @@ const mapDispatchToProps = {
   hideNotification
 }
 
-const connectedAnecdoteForm = connect(null, mapDispatchToProps)(AnecdoteForm)
-
-export default connectedAnecdoteForm
+export default connect(null, mapDispatchToProps)(AnecdoteForm)
